fa879f58331947fa22faea7e4e1604e813e2329d
fanihenry <108129486+fanihenry@users.noreply.github.com>
2023-11-22 14:54:10 +0000

    Lab2_22.11.2023

--- Exercises2023/Ex1_2/AnalyseData copy.cxx
+++ Exercises2023/Ex1_2/AnalyseData copy.cxx
@@ -2,6 +2,17 @@
 // 15 Nov 2023
 // SUPA C++ course - Lab 1
 
+/* Update: I have just split the 2 reading/printing into 2 functions,
+I now need to check if they're well defined and call them separately 
+in the main function 
+
+Note that the *not copy* version of this script is just what I had before 
+splitting the 2 functions 
+=> while this isn't ready to work, keep the old verison as a backup 
+!!!! be careful when compiling/running since it's a different name 
+=> once this is working, delete the old version
+*/
+
 #include <iostream>
 #include <fstream>
 #include <cmath>
@@ -13,6 +24,12 @@ int printing();
 
 
 int main(){
+
+    // call the 2 functions define after 
+}
+
+//reading function
+int reading(){
 
     // open and read the file
     std::string fileName = "input2D_float.txt";
@@ -45,16 +62,19 @@ int main(){
             y = std::stof(y_string);
             y_array.push_back(y);
 
-            //printing the (x,y) coordinates
-            std::cout << "(" << x << "," << y << ")" << std::endl;
-
         }
         lineNumber++;
-        
     }
-
     inputFile.close();
+    return 0;
+}
 
+//printing function
+int printing(){
+    for(i=0, i++, i=sizeof(x_array)){
+        //printing the (x,y) coordinates
+        std::cout << "(" << x << "," << y << ")" << std::endl;
+    }
     return 0;
 }
 
