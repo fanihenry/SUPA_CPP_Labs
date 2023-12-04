35f2c7653707b1410de567493a5a4e2868b95377
fanihenry <108129486+fanihenry@users.noreply.github.com>
2023-12-04 13:35:26 +0000

    work of monday morning - lab1

--- Exercises2023/Ex1_2/CustomFunctions.cxx
+++ Exercises2023/Ex1_2/CustomFunctions.cxx
@@ -5,12 +5,16 @@
 #include <cmath>
 #include <vector>
 #include <numeric>
+#include <algorithm>
 
 // defining vectors outside the scope of the functions
 std::vector<double> x_array;
 std::vector<double> y_array;
 std::vector<double> magnitude_array;
 std::vector<double> deviation_array;
+std::vector<double> gradient_array;
+std::vector<double> intercept_array;
+std::vector<double> sum_array;
 
 //reading function 
 
@@ -68,9 +72,10 @@ int printing(int lineNumber){
         x = x_array[i];
         y = y_array[i];
 
-        //calculating the magnitude and printing thr outcome
+        //calculating the magnitude and printing the outcome
         float magnitude = sqrt(pow(x,2) + pow(y,2));
         std::cout << "For the point " << i << " with coordinates (" << x << "," << y << ")"<< ", the magnitude of the vector is " << magnitude << std::endl;
+        magnitude_array.push_back(magnitude);
     }
     //std::cout << "Printing function is working" << std::endl; //check-point
     return 0;
@@ -97,15 +102,30 @@ int straightLine(){
     float gradient=3;
     float intercept=2;
     float deviation, sum;
+    const int size=10;
+    int gradient_array[size], intercept_array[size];
 
-    // calculations using the least mean square method
-    for(int i=0; i<sizeof(x_array); i++){
-        deviation = sqrt( (gradient * x_array[i] + intercept) - y_array[i]) ;
-        deviation_array.push_back(deviation);
-        std::cout << deviation << std::endl;
+    std::iota(gradient_array, gradient_array+size, 1);
+    std::iota(intercept_array, intercept_array+size, 1);
+
+    // calculations using the least mean square method, looping over various values for the gradient and intercept
+    for (int g=0; g<size; g++){
+        gradient = gradient_array[g];
+
+        for (int n=0; n<size; n++){
+            intercept = intercept_array[n];
+
+            for(int i=0; i<x_array.size(); i++){
+                deviation = pow( ((gradient * x_array[i] + intercept) - y_array[i]) ,2) ;
+                deviation_array.push_back(deviation);
+                std::cout << deviation << std::endl;
+            }
+
+            sum = accumulate(deviation_array.begin(),deviation_array.end(),0.0f);
+            //std::cout << "The sum is " << sum << std::endl;
+            sum_array.push_back(sum);
+            std::cout << "The sum value " << sum << " for gradient " << g << " and intercept " << n << " has been added to the list" << std::endl;
+        }
     }
-    sum = accumulate(deviation_array.begin(),deviation_array.end(),0);
-    std::cout << "The sum is " << sum << std::endl;
-
     return 0;
 }
